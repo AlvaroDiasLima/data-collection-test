fc9b74fd31c68266969604e2add610929d3aec40
AlvaroDiasLima <31149528+AlvaroDiasLima@users.noreply.github.com>
2017-08-20 15:58:39 -0300

    Update tagueamento.js

--- 001/tagueamento.js
+++ 001/tagueamento.js
@@ -1,12 +1,13 @@
   $(document).ready(function() {  
- 	var botoes = [];  //
+    var botoes = [];
  	$.each($('#botoes button'), function(key, value) {  
  		botoes.push($(value).text());  
-            });  
+  });  
 
     
  	$('#botoes button').on('click', function() {  
  		console.log(($(this).text()));  
  	});  
    
- 	console.log(botoes);  
+ 	console.log(botoes);
+    
